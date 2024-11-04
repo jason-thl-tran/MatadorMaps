c135009c1871d550ab86c729d0918cec210380b5
[jasontran tranjason] <87417183+jason-thl-tran@users.noreply.github.com>
2024-11-04 13:07:38 -0600

    Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import { Image, StyleSheet, Platform, View } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import Mapbox from '@rnmapbox/maps';
+
+Mapbox.setAccessToken('pk.eyJ1IjoiamFzb3RyYW4iLCJhIjoiY20zMmRidDZ4MWMxMzJsb3FscHVuMHpxZSJ9.q7SzlZTC0ia3ADO7FvWdcA');
+
+const App = () => {
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <Mapbox.MapView style={styles.map} />
+      </View>
+    </View>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    height: 300,
+    width: 300,
+  },
+  map: {
+    flex: 1
+  }
+});
